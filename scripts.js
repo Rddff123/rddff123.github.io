c627bed490b0209d9270fa7190485e4307527465
Andychan <122155684+Rddff123@users.noreply.github.com>
2023-08-26 11:13:27 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Created on iPad.
+    var now = new Date();
+    var y2k = new Date("April 26, 2028 00:00:00");
+    var seconds = (y2k - now) / 1000;
+    var years = seconds / 3600 / 24 / 365;
+    var roundedYears = Math.round(years);
+    document.getElementById("years").textContent = roundedYears;
+
+    warn("ROBOT ATTACK IS NOT ALLOWED");
+
+    function warn(message) {
+        console.log(message);
+    }
